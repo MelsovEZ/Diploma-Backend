c805f8615cb1beb293e613391856d16c4ce843a0
Rakhimov Asanali <119415319+Asanix@users.noreply.github.com>
2025-02-20 15:44:14 +0500

    Changed

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\api\HomeController::class, 'index']);
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/home', [\App\Http\Controllers\api\HomeController::class, 'index']);
