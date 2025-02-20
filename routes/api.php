cc9440c0124c9e9029fc5204880d44cb5a531073
Rakhimov Asanali <119415319+Asanix@users.noreply.github.com>
2025-02-20 15:33:56 +0500

    Changed

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,8 +14,10 @@ Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'login
 Route::post('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout']);
 Route::get('/user', [\App\Http\Controllers\Api\UserController::class, 'getUser'])->middleware('auth:sanctum');
 
-Route::get('/docs', function () {
-    return redirect('/api/documentation');
+Route::get('/run-command', function () {
+    Artisan::call('l5-swagger:generate');
+    return "Swagger documentation generated!";
 });
+
 
 Route::get('/home', [\App\Http\Controllers\api\HomeController::class, 'index'])->middleware('auth:sanctum');
