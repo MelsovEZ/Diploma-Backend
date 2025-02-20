26051fbecf1d6944ddcb57a7670c865dd030c2ba
Rakhimov Asanali <119415319+Asanix@users.noreply.github.com>
2025-02-20 14:15:50 +0500

    Changed

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,8 @@ Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'login
 Route::post('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout']);
 Route::get('/user', [\App\Http\Controllers\Api\UserController::class, 'getUser'])->middleware('auth:sanctum');
 
+Route::get('/docs', function () {
+    return redirect('/api/documentation');
+});
+
 Route::get('/home', [\App\Http\Controllers\api\HomeController::class, 'index'])->middleware('auth:sanctum');
